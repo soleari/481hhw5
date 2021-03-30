6a880388687c881caa54ba38ed99e1bd15441a98
soleari <67401131+soleari@users.noreply.github.com>
2021-03-30 13:18:17 -0400

    Add files via upload

---
+++ delta.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+size_of_set = sys.argv[1]
+#interesting_function = sys.argv[2]
+
+starting_list = list( range (int(size_of_set))
+print(starting_list)
+
+list_string = " ".join( map(str, starting_list))
+print(list_string)
